6ebb388506603fd13904d34eedbdf14282ac0790
imkanaram <kanaramyadavofficial@gmail.com>
2020-07-16 21:11:38 +0530

    checkout page

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product, Contact
+from .models import Product, Contact, Orders
 from math import ceil
 from django.http import HttpResponse
 
@@ -53,4 +53,10 @@ def search(request):
 
 
 def checkout(request):
-    return render(request, 'shop/checkout.html')
+    name = request.POST.get('name', '')
+    email = request.POST.get('email', '')
+    phone = request.POST.get('phone', '')
+    desc = request.POST.get('desc', '')
+    contact = Contact(name=name, email=email, phone=phone, desc=desc)
+    contact.save()
+    return render(request, 'shop/checkout1.html')
