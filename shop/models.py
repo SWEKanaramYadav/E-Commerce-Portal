7f81d3ba7a10c39857cc08523c288c48e7ca8202
imkanaram <kanaramyadavofficial@gmail.com>
2020-07-18 10:49:11 +0530

    final checkout and edit in models

--- shop/models.py
+++ shop/models.py
@@ -26,13 +26,20 @@ class Contact(models.Model):
 class Orders(models.Model):
     order_id = models.AutoField(primary_key=True)
     items_json = models.CharField(max_length=5000)
-    fname = models.CharField(max_length=90)
-    lname = models.CharField(max_length=90)
+    name = models.CharField(max_length=90)
     email = models.CharField(max_length=200)
     phone = models.CharField(max_length=90)
     address = models.CharField(max_length=300)
-    address2 = models.CharField(max_length=500)
     city = models.CharField(max_length=200)
     state = models.CharField(max_length=200)
     zip_code = models.CharField(max_length=90)
+
+class OrderUpdate(models.Model):
+    update_id = models.AutoField(primary_key=True)
+    order_id = models.IntegerField(default="")
+    update_desc = models.CharField(max_length=5000)
+    timestamp = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.update_desc[0:10] + "..."
 
--- shop/views.py
+++ shop/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
-from .models import Product, Contact, Orders
+from .models import Product, Contact, Orders, OrderUpdate
 from math import ceil
+import json
 from django.http import HttpResponse
 
 
 def index(request):
-    # products = Product.objects.all()
-    # n = len(products)
-    # nslides= n//4 + ceil((n/4)-(n//4))
-    # allprods = [[products, range(1, nslides), nslides],
-    #             [products, range(1, nslides), nslides]]
     allprods = []
     catprods = Product.objects.values('category', 'id')
     cats = {item['category'] for item in catprods}
@@ -18,7 +14,6 @@ def index(request):
         n = len(prod)
         nslides = n // 4 + ceil((n / 4) - (n // 4))
         allprods.append([prod, range(1, nslides), nslides])
-    # params = {'no_of_slide':nslides,'range':range(1,nslides),'product':products}
     params = {'allprods': allprods}
     return render(request, 'shop/index.html', params)
 
@@ -39,6 +34,22 @@ def contact(request):
 
 
 def tracker(request):
+    if request.method == "POST":
+        orderId = request.POST.get('orderId','')
+        email = request.POST.get('email','')
+        try:
+            order = Orders.objects.filter(order_id=orderId, email=email)
+            if len(order)>0:
+                update = OrderUpdate.objects.filter(order_id=orderId)
+                updates = []
+                for item in update:
+                    updates.append({'text':item.update_desc, 'timestamp':item.timestamp})
+                    response = json.dumps(updates)
+                    return HttpResponse(response)
+            else:
+                pass
+        except Exception as e:
+            pass
     return render(request, 'shop/tracker.html')
 
 
@@ -53,10 +64,21 @@ def search(request):
 
 
 def checkout(request):
-    name = request.POST.get('name', '')
-    email = request.POST.get('email', '')
-    phone = request.POST.get('phone', '')
-    desc = request.POST.get('desc', '')
-    contact = Contact(name=name, email=email, phone=phone, desc=desc)
-    contact.save()
+    if request.method =="POST":
+        items_json = request.POST.get('itemsJson', '')
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        address = request.POST.get('address1', '') + " " + request.POST.get('address2', '')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        zip_code = request.POST.get('zip_code', '')
+        order = Orders(items_json=items_json, name=name, email=email, phone=phone, address=address,
+                       city=city, state=state, zip_code=zip_code)
+        order.save()
+        update = OrderUpdate(order_id=order.order_id, update_desc="The order has been placed" )
+        update.save()
+        thank = True
+        id = order.order_id
+        return render(request, 'shop/checkout1.html',{'thank':thank, 'id':id})
     return render(request, 'shop/checkout1.html')
