791b33803ce062021bef94259693a0bd82cc8c01
imkanaram <kanaramyadavofficial@gmail.com>
2020-07-21 19:52:39 +0530

    tracker update

--- shop/views.py
+++ shop/views.py
@@ -34,22 +34,23 @@ def contact(request):
 
 
 def tracker(request):
-    if request.method == "POST":
-        orderId = request.POST.get('orderId','')
-        email = request.POST.get('email','')
+    if request.method=="POST":
+        orderId = request.POST.get('orderId', '')
+        email = request.POST.get('email', '')
         try:
             order = Orders.objects.filter(order_id=orderId, email=email)
             if len(order)>0:
                 update = OrderUpdate.objects.filter(order_id=orderId)
                 updates = []
                 for item in update:
-                    updates.append({'text':item.update_desc, 'timestamp':item.timestamp})
-                    response = json.dumps(updates)
-                    return HttpResponse(response)
+                    updates.append({'text': item.update_desc, 'time': item.timestamp})
+                    response = json.dumps([updates, order[0].items_json], default=str)
+                return HttpResponse(response)
             else:
-                pass
+                return HttpResponse('{}')
         except Exception as e:
-            pass
+            return HttpResponse('{}')
+
     return render(request, 'shop/tracker.html')
 
 
